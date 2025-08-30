7e33a7803325532f9918e5426d8a3c340f894105
Pawani328 <pawanih.22@cse.mrt.ac.lk>
2025-08-30 09:47:57 +0530

    auth changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const db = require("../db/init");
+const {db} = require("../db/init");
 const { sendEmail } = require("../utils/mailer");
 
 const router = express.Router();
